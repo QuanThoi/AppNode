d80385fceb867ab11dd4d82f28e3f31ee37e3826
Thoi <thoi.thoi1234.tn@gmail.com>
2018-09-22 15:07:53 +0700

    thoi
    e/import

---
+++ Mod.js
@@ -0,0 +1,12 @@
+function display_mess() {
+	console.log("Message!!!");
+}
+
+function sum(a=1,b=2){
+	return a+b;
+}
+
+function display_message() { 
+   console.log("Hello World") 
+} 
+export {display_message,sum}
---
+++ main.js
@@ -0,0 +1,2 @@
+import {display_message,sum} from './Mod.js' 
+display_message() 
