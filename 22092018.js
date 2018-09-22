01068d81cbda87dbdaf3971873128ec93af66256
Thoi <thoi.thoi1234.tn@gmail.com>
2018-09-22 08:29:47 +0700

    thoi
    a

--- 22092018.js
+++ 22092018.js
@@ -1,8 +1,15 @@
-var number = 10;
-function test() {
-	let number = 100;
-	console.log("Bien cuc bo la: "+ number);
+// var number = 10;
+// function test() {
+// 	let number = 100;
+// 	console.log("Bien cuc bo la: "+ number);
+// }
+// console.log("Bien toan cuc la: "+number);
+// test();
+let main = function(){
+	var n = 5;
+	for (var i = 0; i <= n; i++) {
+		console.log("Gia tri i trong for la: "+i);
+	}
+	console.log("main la 1 hosting");
 }
-console.log("Bien toan cuc la: "+number);
-test();
-
+main();
